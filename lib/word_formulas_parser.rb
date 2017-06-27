bc7ee65b3e664a4055fb7a061dae9b70235a61e2
SergeiKaptelin <serhii.kaptelin@gmail.com>
2017-06-27 18:59:57 +0300

    add check to aditional programs in system

--- lib/word_formulas_parser.rb
+++ lib/word_formulas_parser.rb
@@ -2,13 +2,22 @@ require 'word_formulas_parser/version'
 require 'word_formulas_parser/docx'
 
 module WordFormulasParser
-  class DocxParser
     def self.detect_and_parse(input_file_path, output_file_path)
+      test_requirements!
       extencion = File.extname(input_file_path)
       case extencion
       when /\.docx/i
         then Docx.parse(input_file_path, output_file_path)
       end
     end
-  end
+
+    def self.test_requirements!
+      raise 'LibbreOffice is not installed' if !system("which soffice")
+
+      raise 'writer2latex is not installed' if !system("which w2l")
+
+      raise 'latex is not installed' if !system("which latex")
+
+      raise 'dvipng is not installed' if !system("which dvipng")
+    end
 end
