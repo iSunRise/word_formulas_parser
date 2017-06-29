3b31a770a110a67fb3757ed50ab685d864e5a15f
SergeiKaptelin <serhii.kaptelin@gmail.com>
2017-06-29 12:12:33 +0300

    refactoring, add error management for system commands

--- lib/word_formulas_parser/docx.rb
+++ lib/word_formulas_parser/docx.rb
@@ -6,49 +6,49 @@ require 'securerandom'
 module WordFormulasParser
   class Docx
     class << self
+      def process(input_file_path, output_file_path)
+        begin
+          parse(input_file_path, output_file_path)
+        rescue => ex
+          puts "#{ex.class}:#{ex}"
+        end
+      end
+
       def parse(input_file_path, output_file_path)
+        # for formulas like $ %tex_formula% $
+        regexp_1 = /\$(.*?)\$/im
+
+        # for formulas like \begin{equation*}
+        #                    %tex_formula%
+        #                   \end{equation*}
+        regexp_2 = /\\begin{equation\*}(.+?)\\end{equation\*}/im
+
         outdir, docx_file_name = File.split(input_file_path)
 
         # create .odt from .docx
-        system("soffice --headless --convert-to odt --outdir #{outdir} #{input_file_path}")
+        sucess = system("soffice --headless --convert-to odt --outdir #{outdir} \
+          #{input_file_path}")
+         # binding.pry
+        raiser(' soffice converting from .docx to .odt failed', sucess)
 
         # get name without .docx
-        extencion = File.extname(docx_file_name)
-        name = docx_file_name.gsub(extencion, '')
+        extention = File.extname(docx_file_name)
+        name = docx_file_name.gsub(extention, '')
 
         # create path to odt and tex files
         odt_file_path = File.expand_path(name + ".odt", outdir)
         tex_file_path = File.expand_path(name + ".tex", outdir)
 
         # create .tex from .odt
-        system("w2l #{odt_file_path} #{tex_file_path}")
+        sucess = system("w2l #{odt_file_path} #{tex_file_path}")
+        raiser(' w2l converting from .odt to .tex failed', sucess)
 
         # create array with formulas
         tex_text = File.read(tex_file_path)
 
         formulas = []
-
-        # find formula like $ tex_formula $
-        formulas_array_v1 = tex_text.scan(/\$(.*?)\$/im)
-        if formulas_array_v1.any?
-          formulas_array_v1.each do |formulas_array|
-            formulas_array.each do |formula|
-              formulas << formula
-            end
-          end
-        end
-
-        # find formula like \begin{equation*}
-        #                    tex_formula
-        #                   \end{equation*}
-        formulas_array_v2 = tex_text.scan(/\\begin{equation\*}(.+?)\\end{equation\*}/im)
-        if formulas_array_v2.any?
-          formulas_array_v2.each do |formulas_array|
-            formulas_array.each do |formula|
-              formulas << formula
-            end
-          end
-        end
+        formulas += formulas_finder(regexp_1, tex_text)
+        formulas += formulas_finder(regexp_2, tex_text)
 
         images = formulas_to_png(formulas)
 
@@ -69,6 +69,19 @@ module WordFormulasParser
 
       private
 
+
+      def raiser(error, sucess)
+        raise error unless sucess
+      end
+
+      def formulas_finder(regexp, tex_text)
+        formulas = []
+        tex_text.scan(regexp) do |match_arr|
+          match_arr.each { |formula|  formulas << formula }
+        end
+        formulas
+      end
+
       # Renders expression to PNG image using <tt>latex</tt> and <tt>dvipng</tt>
       # returns array with paths to images
       def formulas_to_png(formulas, background = 'White', density = 700)
@@ -83,19 +96,22 @@ module WordFormulasParser
             # .tex file that must be converted to .png
             File.open("#{sha1}.tex", 'w') do |f|
               f.write("\\documentclass{article}\n \
-                      \\usepackage{mathtext}\n \
-                      \\usepackage[T2A]{fontenc}\n \
-                      \\usepackage[utf8]{inputenc}\n \
-                      \\usepackage[english, russian]{babel}\n \
-                      \\usepackage{amsmath,amssymb}\n \
-                      \\begin{document}\n \
-                      \\thispagestyle{empty}\n \
-                      $$ #{formula} $$\n \
-                      \\end{document}\n")
+                       \\usepackage{mathtext}\n \
+                       \\usepackage[T2A]{fontenc}\n \
+                       \\usepackage[utf8]{inputenc}\n \
+                       \\usepackage[english, russian]{babel}\n \
+                       \\usepackage{amsmath,amssymb}\n \
+                       \\begin{document}\n \
+                       \\thispagestyle{empty}\n \
+                         $$ #{formula} $$\n \
+                       \\end{document}\n")
             end
 
             # create .png from .tex
-            sucess = system("latex -interaction=nonstopmode #{sha1}.tex && dvipng -q -T tight -bg #{background} -D #{density.to_i} -o #{sha1}.png #{sha1}.dvi")
+            sucess = system("latex -interaction=nonstopmode #{sha1}.tex && \
+                            dvipng -q -T tight -bg #{background} \
+                            -D #{density.to_i} -o #{sha1}.png #{sha1}.dvi")
+            raiser(' latex converting to .png failed', sucess)
 
             # deleting unused files
             ["#{sha1}.tex", "#{sha1}.aux", "#{sha1}.dvi", "#{sha1}.log"].each do |file|
@@ -106,6 +122,7 @@ module WordFormulasParser
             arr_images << File.join(temp_path, "#{sha1}.png") if sucess
           end
         end
+
         return arr_images
       end
 
